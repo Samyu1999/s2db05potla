26bdd7fffd21420831fdae9674f219048d5223f6
Samyu1999 <samyukthap09@gmail.com>
2022-04-06 15:30:31 -0500

    scrnshot 7 updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ovenRouter = require('./routes/oven');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/oven', ovenRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 
 
